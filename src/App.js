475d1bdf732539f3311fe5fb0955f0696ba82a11
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2021-01-20 23:30:30 +0000

    creating different page routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import { NavbarBrand } from "react-bootstrap";
-import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
-import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import Nav from "react-bootstrap/Nav";
 
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ContactPage from "./pages/ContactPage";
 
 export default class App extends Component {
   constructor(props) {
@@ -23,7 +24,7 @@ export default class App extends Component {
       ],
       home: {
         title: "Be relentless",
-        subtitle: "Projects that make a difference",
+        subTitle: "Projects that make a difference",
         text: "Checkout my projects"
       },
       about: {
@@ -61,6 +62,32 @@ export default class App extends Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            exact
+            render={() => <AboutPage title={this.state.about.title} />}
+          />
+          <Route
+            path="/projects"
+            exact
+            render={() => <ProjectsPage title={this.state.projects.title} />}
+          />
+          <Route
+            path="/contact"
+            exact
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
           <Footer />
         </Container>
       </Router>
